ef688be82c29f7fa55fa1b338a5e5cf2da28c79e
vagrant <vagrant@localhost.localdomain>
2020-06-10 06:20:47 +0000

    finish advanced assignment3

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,7 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
     @book_new = Book.new
     @user = @book.user
+    @book_comment = BookComment.new
   end
 
   def index
@@ -34,8 +35,6 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-
-
 
   def update
   	@book = Book.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'home/about'
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books, except: [:new]
+
+  resources :books, except: [:new] do
+  	resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
+  end
 end
